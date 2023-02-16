e9960fd4a90544f3f1b7399019237741550612ac
Stickki <106401702+Stickki@users.noreply.github.com>
2023-02-16 16:38:55 +0100

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,13 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    private int[] arr = new int[20];;
+    private int index = 0;
+    public int[] Calculate(int A, int B){
+        for(int i = A; i <= B; i++){
+            arr[index] = i;
+            index++;
+        }
+        return arr;
     }
 }
