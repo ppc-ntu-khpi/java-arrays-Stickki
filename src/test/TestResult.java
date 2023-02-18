2d15b5ace5ecc78430ebc4e42fdb5c4e8bc92f75
Stickki <106401702+Stickki@users.noreply.github.com>
2023-02-18 22:18:20 +0100

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,16 +5,16 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-        int a = 15;
-        int b = 23;
-        Exercise exercise = new Exercise();
-        int[] array = exercise.Calculate(a, b);
-        for(int i = 0; a <= b; i++){
-            if(array[i] == 0){
+        int a = 15; // початок діапазну
+        int b = 23; // і кінець
+        Exercise exercise = new Exercise(); // створюємо об'єкт класа Exercise
+        int[] array = exercise.Calculate(a, b); // передаємо наш діапазон в метод Calculate і присвоюємо повертаєме значення цього методу в масив array
+        for(int i = 0; a <= b; i++){ // цей цикл просто виводить всі значення масиву 
+            if(array[i] == 0){ // доки елемент масиву під відповідним індексом не дорівнює 0
                 break;
             }
             System.out.println(array[i]);
         }
-        
+
     }
 }
